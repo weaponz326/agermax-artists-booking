b4bfa4a28ea3c04bc83587206de9b151a8ba333d
Russel D. Boakye <113293972+blingyplus@users.noreply.github.com>
2023-12-28 11:35:00 +0000

    fix console error

---
+++ client/src/pages/admin/artist/ArtistAccountProfile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArtistAccountProfile extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ArtistAccountProfile;
---
+++ client/src/pages/admin/artist/ArtistAccountSettings.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ArtistAccountSettings extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ArtistAccountSettings;
