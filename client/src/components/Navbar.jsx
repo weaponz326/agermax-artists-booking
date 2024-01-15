9343ef2dc2bf81a14546e9737fcd74537bd695e4
Charles Owusu <charlesowusu274@gmail.com>
2024-01-15 19:36:14 +0000

    Nav Bar desktop version milestone

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,14 +1,49 @@
 import { FaSearch } from 'react-icons/fa'
 import { IconContext } from 'react-icons'
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Navbar() {
   const [navItemsOpen, setNavItemsOpen] = useState(false)
   const [selectNavItem, setSelectNavItem] = useState('')
+  const [navItemsDetails, setNavItemsDetails] = useState(null)
+  const navBarRef = useRef()
+
+  useEffect(() => {
+    const handleClickOutside = e => {
+      if (navBarRef.current && !navBarRef.current.contains(e.target)) {
+        setNavItemsOpen(false)
+        setNavItemsDetails(null)
+        setSelectNavItem('')
+
+        console.log(navBarRef.current)
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [navBarRef])
+
+  useEffect(() => {
+    if (selectNavItem === 'Events') {
+      setNavItemsDetails(() => {
+        return <div className='nav-item-detail'>Events Coming Soon. Stay Tuned!</div>
+      })
+    } else if (selectNavItem === 'Performers') {
+      setNavItemsDetails(currentItem => {
+        return <Search displayNavItems={displayNavItems} />
+      })
+    } else if (selectNavItem === 'Articles') {
+      setNavItemsDetails(() => {
+        return <div className='nav-item-detail'>Very soon, this will come also. Stay tuned!</div>
+      })
+    }
+  }, [selectNavItem])
 
   const displayNavItems = () => {
     setNavItemsOpen(true)
-    setActiveNavItem('Search')
+    setSelectNavItem('Performers')
   }
 
   const setActiveNavItem = activeNavItem => {
@@ -16,37 +51,13 @@ export default function Navbar() {
   }
 
   const showNavItems = navItemsOpen ? (
-    <NavItems setActiveNavItem={setActiveNavItem} />
+    <NavItems setActiveNavItem={setActiveNavItem} activeNavItem={selectNavItem} />
   ) : (
     <Search displayNavItems={displayNavItems} />
   )
 
-  function switchNavItemDetails() {
-    if (selectNavItem === 'Search') {
-      {
-        return <Search displayNavItems={displayNavItems} />
-      }
-    } else if (selectNavItem === 'Events') {
-      return (
-        <div className='nav-item-detail'>
-          Events Coming Soon
-          <br />
-          Stay Tuned!
-        </div>
-      )
-    } else if (selectNavItem === 'Articles') {
-      return (
-        <div className='nav-item-detail'>
-          Articles also Coming Soon
-          <br />
-          Stay Tuned!
-        </div>
-      )
-    }
-  }
-
   return (
-    <nav className='header-navbar'>
+    <nav className='header-navbar' ref={navBarRef}>
       <nav className='top-bar nav-bar'>
         <div className='logo first-img'>
           <img className='logo-img ' alt='App dark' src='/images/logo.png' />
@@ -56,54 +67,57 @@ export default function Navbar() {
           <img className='logo-img ' alt='Ellipse' src='/images/ellipse-121.png' />
         </div>
       </nav>
-      {switchNavItemDetails()}
+      {navItemsDetails}
     </nav>
   )
 }
 
-export const NavItems = ({ setActiveNavItem }) => {
+export const NavItems = ({ setActiveNavItem, activeNavItem }) => {
   return (
     <div className='nav-items'>
-      <Performers setActiveNavItem={setActiveNavItem} />
-      <Events setActiveNavItem={setActiveNavItem} />
-      <Articles setActiveNavItem={setActiveNavItem} />
+      <Performers setActiveNavItem={setActiveNavItem} activeNavItem={activeNavItem} />
+      <Events setActiveNavItem={setActiveNavItem} activeNavItem={activeNavItem} />
+      <Articles setActiveNavItem={setActiveNavItem} activeNavItem={activeNavItem} />
     </div>
   )
 }
 
-export const Performers = ({ setActiveNavItem }) => {
+export const Performers = ({ setActiveNavItem, activeNavItem }) => {
+  const setActiveClass = activeNavItem === 'Performers' ? 'nav-item active-nav-item' : 'nav-item'
+
   return (
-    <span className='nav-item' onClick={() => setActiveNavItem('Search')}>
+    <span className={setActiveClass} onClick={e => setActiveNavItem(e.target.textContent)}>
       Performers
     </span>
   )
 }
-export const Events = ({ setActiveNavItem }) => {
+export const Events = ({ setActiveNavItem, activeNavItem }) => {
+  const setActiveClass = activeNavItem === 'Events' ? 'nav-item active-nav-item' : 'nav-item'
+
   return (
-    <span className='nav-item' onClick={() => setActiveNavItem('Events')}>
+    <span className={setActiveClass} onClick={e => setActiveNavItem(e.target.textContent)}>
       Events
     </span>
   )
 }
-export const Articles = ({ setActiveNavItem }) => {
+export const Articles = ({ setActiveNavItem, activeNavItem }) => {
+  const setActiveClass = activeNavItem === 'Articles' ? 'nav-item active-nav-item' : 'nav-item'
+
   return (
-    <span className='nav-item' onClick={() => setActiveNavItem('Articles')}>
-      Articles{' '}
+    <span className={setActiveClass} onClick={e => setActiveNavItem(e.target.textContent)}>
+      Articles
     </span>
   )
 }
 
 const Search = ({ displayNavItems }) => {
-  const [openMenu, setOpenMenu] = useState(false)
-  const inputRef = useRef()
-
   function handleClick() {
     displayNavItems()
   }
 
   return (
     <div className='search-bar'>
-      <input type='search' name='' id='' placeholder='Find amazing artists' ref={inputRef} onClick={handleClick} />
+      <input type='search' name='' id='' placeholder='Find amazing artists' autoFocus onClick={handleClick} />
       <IconContext.Provider value={{ color: 'white', size: '1.2rem' }}>
         <div className='search-icon'>
           <FaSearch />
