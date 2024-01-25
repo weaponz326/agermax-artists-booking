b22f62614a7d669741d48b8dced532b9a09260f9
Russel D. Boakye <113293972+blingyplus@users.noreply.github.com>
2024-01-25 12:32:48 +0000

    display navlinks based on user role

--- client/src/navigation/vertical/index.js
+++ client/src/navigation/vertical/index.js
@@ -1,36 +1,47 @@
+import { useAuth } from 'src/hooks/useAuth'
+
 const navigation = () => {
-  return [
+  const { user } = useAuth()
+
+  const navItems = [
     {
       title: 'Home',
       path: '/admin/home',
-      icon: 'tabler:smart-home',
+      icon: 'tabler:smart-home'
     },
     {
       title: 'Bookings',
       path: '/admin/bookings',
-      icon: 'tabler:address-book',
+      icon: 'tabler:address-book'
     },
     {
       title: 'Inbox',
       path: '/admin/inbox',
-      icon: 'tabler:mail',
+      icon: 'tabler:mail'
     },
-    {
-      title: 'Users',
-      path: '/admin/users',
-      icon: 'tabler:users',
-    },
+    // Conditionally include the 'Users' link if the user is an admin
+    ...(user && user.role === 'admin'
+      ? [
+          {
+            title: 'Users',
+            path: '/admin/users',
+            icon: 'tabler:users'
+          }
+        ]
+      : []),
     {
       title: 'Finance',
       path: '/admin/finance',
-      icon: 'tabler:report-money',
+      icon: 'tabler:report-money'
     },
     {
       path: '/admin/account',
       title: 'Account',
-      icon: 'tabler:user-circle',
+      icon: 'tabler:user-circle'
     }
   ]
+
+  return navItems
 }
 
 export default navigation
