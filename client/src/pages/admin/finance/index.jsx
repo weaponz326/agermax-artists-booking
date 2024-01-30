b3e2e5b27a20c09396d43409f20e815224a94910
Charles Owusu <charlesowusu274@gmail.com>
2024-01-30 08:19:11 +0000

    Table for payment orders and dynamic linking to the details page and styling

--- client/src/pages/admin/finance/index.jsx
+++ client/src/pages/admin/finance/index.jsx
@@ -1,7 +1,5 @@
 import styles from './Finance.module.css'
 import React, { useState } from 'react'
-import { Table } from 'antd'
-import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 const Finance = () => {
@@ -19,14 +17,14 @@ const data = [
     paymentId: 'P001',
     payee: 'Event Organizer A',
     amount: 1500,
-    date: '2022-02-15',
+    date: '2024-01-30',
     status: 'Paid'
   },
   {
     paymentId: 'P002',
     payee: 'Event Organizer B',
-    amount: 1500,
-    date: '2022-02-15',
+    amount: 8500,
+    date: '2024-02-18',
     status: 'Pending'
   }
   // add more payment objects here
@@ -88,73 +86,6 @@ export const ViewDetailsAction = ({ id, payee, amount, date, status }) => {
     </div>
   )
 }
-// export const FinancialTable = () => {
-//   const router = useRouter()
-
-//   // Sample data for the financial table
-//   const data = [
-//     {
-//       paymentId: 'P001',
-//       payee: 'Event Organizer A',
-//       amount: 1500,
-//       date: '2022-02-15',
-//       status: 'Paid',
-//       action: <ViewDetailsAction id={paymentId} />
-//     },
-//     {
-//       paymentId: 'P002',
-//       payee: 'Event Organizer B',
-//       amount: 2000,
-//       date: '2022-02-18',
-//       status: 'Pending'
-//       // action: <ViewDetailsAction id={this.paymentId} />
-//     }
-//     // Add more data as needed
-//   ]
-
-//   // Define columns for the table
-//   const columns = [
-//     {
-//       title: 'Payment ID',
-//       dataIndex: 'paymentId',
-//       key: 'paymentId',
-//       sorter: (a, b) => a.paymentId.localeCompare(b.paymentId)
-//     },
-//     {
-//       title: 'Payee',
-//       dataIndex: 'payee',
-//       key: 'payee',
-//       sorter: (a, b) => a.payee.localeCompare(b.payee)
-//     },
-//     {
-//       title: 'Amount',
-//       dataIndex: 'amount',
-//       key: 'amount',
-//       sorter: (a, b) => a.amount - b.amount,
-//       render: amount => `$${amount.toFixed(2)}` // Format amount as currency
-//     },
-//     {
-//       title: 'Date',
-//       dataIndex: 'date',
-//       key: 'date',
-//       sorter: (a, b) => new Date(a.date) - new Date(b.date)
-//     },
-//     {
-//       title: 'Status',
-//       dataIndex: 'status',
-//       key: 'status',
-//       sorter: (a, b) => a.status.localeCompare(b.status)
-//     },
-//     {
-//       title: 'Action',
-//       dataIndex: 'action',
-//       key: 'action',
-//       sorter: (a, b) => a.action.localeCompare(b.action)
-//     }
-//   ]
-
-//   return <Table dataSource={data} columns={columns} />
-// }
 
 Finance.authGuard = false
 Finance.guestGuard = false
