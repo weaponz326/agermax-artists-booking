1ff681efce4006e611602d9f611a26f31b9e88c2
Russel D. Boakye <113293972+blingyplus@users.noreply.github.com>
2024-01-31 08:52:37 +0000

    stripe payment backend and frontend

--- client/src/pages/admin/finance/organizer/details/stripe-payment/[paymentId].jsx
+++ client/src/pages/admin/finance/organizer/details/stripe-payment/[paymentId].jsx
@@ -1,30 +1,25 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from 'src/components/CheckoutForm/CheckoutForm'; // Adjust the import path as necessary
+import Box from '@mui/material/Box';
 
-const StripePaymentPage = () => {
-  const router = useRouter()
-  console.log(router.query)
-  const { paymentId, payee, amount, date, status } = router.query
+// Load your publishable key from env variables or directly (ensure it's the public key)
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+const StripePayment = () => {
   return (
-    <div>
-      <p>This is all Generated dynamically from the payment page!</p>
-      <h2>Payee: {payee}</h2>
-      <h3>Payment ID: {paymentId}</h3>
-      <h4>Amount: {amount}</h4>
-      <p>Date: {date}</p>
-      <p>Status: {status}</p>
-      <p>Not doing so bad for my first time out doing web development right!🤣🤣</p>
-    </div>
-  )
-}
+    <Elements stripe={stripePromise}>
+      <Box
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"         sx={{
+          p: 3         }}
+      >
+        <CheckoutForm />
+      </Box>
+    </Elements>
+  );
+};
 
-export default StripePaymentPage
-
-StripePaymentPage.authGuard = false
-StripePaymentPage.guestGuard = false
-StripePaymentPage.acl = {
-  action: 'manage',
-  subject: 'all' // Adjust the permissions as per your application's ACL configuration
-}
-
-// StripePaymentPage.getLayout = page => <div>{page}</div>
+export default StripePayment;
