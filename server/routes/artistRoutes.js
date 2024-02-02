0f6c855f67420cfb5493f5c74211b07f9b862cba
Weps <weaponz326@gmail.com>
2024-02-02 11:10:51 +0000

    image upload api endpoints added

--- server/routes/artistRoutes.js
+++ server/routes/artistRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const multer  = require('multer');
 const router = express.Router();
 const artistController = require('../controllers/artistController');
 
+const profilePhotoUpload = multer({ dest: 'uploads/user/profile_photo/' });
+const galleryUpload = multer({ dest: 'uploads/user/artist_gallery/' });
+
 router.get('/artists', artistController.getAllArtists);
 router.get('/artists/:id', artistController.getArtistById);
-router.post('/artists', artistController.createArtist);
-router.put('/artists/:id', artistController.updateArtist);
+router.post('/artists', profilePhotoUpload.single('profilePhoto'), galleryUpload.array('gallery', 10), artistController.createArtist);
+router.put('/artists/:id', profilePhotoUpload.single('profilePhoto'), galleryUpload.array('gallery', 10), artistController.updateArtist);
 router.delete('/artists/:id', artistController.deleteArtist);
 
 module.exports = router;
--- server/routes/eventOrganizerRoutes.js
+++ server/routes/eventOrganizerRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const multer  = require('multer');
 const router = express.Router();
 const organizerController = require('../controllers/eventOrganizerController');
 
+const profilePhotoUpload = multer({ dest: 'uploads/user/profile_photo/' });
+
 router.get('/organizers', organizerController.getAllOrganizers);
 router.get('/organizers/:id', organizerController.getOrganizerById);
-router.post('/organizers', organizerController.createOrganizer);
-router.put('/organizers/:id', organizerController.updateOrganizer);
+router.post('/organizers', profilePhotoUpload.single('profilePhoto'), organizerController.createOrganizer);
+router.put('/organizers/:id', profilePhotoUpload.single('profilePhoto'), organizerController.updateOrganizer);
 router.delete('/organizers/:id', organizerController.deleteOrganizer);
 
 module.exports = router;
