9ac2978a46a234e194b476f737d8f4564c2a5ec9
Russel D. Boakye <113293972+blingyplus@users.noreply.github.com>
2024-02-23 22:01:27 +0000

    fix payment

--- client/src/services/payments.js
+++ client/src/services/payments.js
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
 const baseUrl = process.env.NEXT_PUBLIC_API_URL
-export async function getAllPayments() {
+export async function getAllPayment() {
   try {
     const { data } = await axios.get(`${baseUrl}/payments`)
-    const sortedPaymentsByDate = data.sort((a, b) => new Date(a.dateTimeRequested) - new Date(b.dateTimeRequested))
-    return sortedPaymentsByDate
+    const sortedPaymentByDate = data.sort((a, b) => new Date(a.dateTimeRequested) - new Date(b.dateTimeRequested))
+    return sortedPaymentByDate
   } catch (error) {
     console.log('Error: ', error)
   }
 }
 
-export async function getPaymentsById(id) {
+export async function getPaymentById(id) {
   try {
     const { data } = await axios.get(`${baseUrl}/payments/${id}`)
     return data
@@ -20,22 +20,13 @@ export async function getPaymentsById(id) {
   }
 }
 
-export async function createPayments(paymentsData) {
+export async function createPayment(paymentData) {
   try {
-    const response = await axios.post(`${baseUrl}/payments`, paymentsData)
+    const response = await axios.post(`${baseUrl}/process-payment`, paymentData)
     return response.data
   } catch (error) {
-    console.log('Error creating payments: ', error)
+    console.log('Error creating payment: ', error)
     throw error // Re-throwing the error so that it can be handled where the function is called
   }
 }
 
-export async function updatePayments(paymentsData) {
-  try {
-    const response = await axios.put(`${baseUrl}/payments/${paymentsData._id}`, paymentsData)
-    return response.data
-  } catch (error) {
-    console.log('Error Updating payments: ', error)
-    throw error // Re-throwing the error so that it can be handled where the function is called
-  }
-}
