fda75e672417d587d779b3f337c09230e0363416
Weps <weaponz326@gmail.com>
2024-03-07 04:53:03 +0000

    user file uploads filenames

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,8 +5,29 @@ const { protect } = require("../middleware/authMiddleware");
 const multer = require("multer");
 
 // Multer configuration for handling file uploads
-const profilePhotoUpload = multer({ dest: "uploads/user/profile_photo/" });
-const galleryUpload = multer({ dest: "uploads/user/artist_gallery/" });
+const profilePhotoStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/user/profile_photo/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+  }
+});
+
+const profilePhotoUpload = multer({ storage: profilePhotoStorage });
+
+const galleryStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/user/artist_gallery/');
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+  }
+});
+
+const galleryUpload = multer({ storage: galleryStorage });
 
 // Middleware for parsing JSON and multipart form data
 router.use(express.json());
